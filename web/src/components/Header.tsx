f08c0999d4854f72e5917b013373ca722d81ab07
exepirit <exepir1t@protonmail.com>
2024-05-01 12:43:03 +0700

    feat: tailwindcss

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -9,9 +9,6 @@ export function Header() {
 				<a href="/" class={url == '/' && 'active'}>
 					Home
 				</a>
-				<a href="/404" class={url == '/404' && 'active'}>
-					404
-				</a>
 			</nav>
 		</header>
 	);
